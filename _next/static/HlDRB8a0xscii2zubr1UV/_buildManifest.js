fcf00f6609547973e55e2dbf442d455ee4724063
sakelog <sakelog@users.noreply.github.com>
2022-04-09 07:25:21 +0000

    deploy: 22fa05da1f0f0cf5a0541cc9142e511a7cfef27f

---
+++ _next/static/HlDRB8a0xscii2zubr1UV/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/1bfc9850-7d1980c0315ede3e.js","static/chunks/712-4e1af48bcf6c5b72.js","static/chunks/pages/index-014067e5836a394d.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
